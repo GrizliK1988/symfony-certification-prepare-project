8427e2b3a9811ff3b6a4b601e6770520d09c69b9
Dmitry Grachikov <dmitry.grachikov@returnonintelligence.com>
2015-10-13 20:30:22 +0300

    Refactoring

--- app/init.php
+++ app/init.php
@@ -8,15 +8,24 @@
 
 namespace DG\App;
 
+use DG\SymfonyCert\Controller\CustomControllerResolver;
 use Symfony\Bridge\Twig\Extension\FormExtension;
 use Symfony\Bridge\Twig\Extension\TranslationExtension;
 use Symfony\Bridge\Twig\Form\TwigRenderer;
 use Symfony\Bridge\Twig\Form\TwigRendererEngine;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Session\Attribute\NamespacedAttributeBag;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\Storage\Handler\NativeFileSessionHandler;
 use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Event\PostResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Translation\TranslatorInterface;
 
 function initSession()
@@ -50,4 +59,32 @@ function initTwig(ContainerInterface $container, TranslatorInterface $translator
     $container->set('twig', $twig);
 
     return $twig;
+}
+
+function initKernel(ContainerInterface $container)
+{
+    /** @var EventDispatcherInterface $eventDispatcher */
+    $eventDispatcher = $container->get('event_dispatcher');
+    $kernel = new HttpKernel($eventDispatcher, new CustomControllerResolver($container));
+
+    $stat = [];
+    $eventDispatcher->addListener(KernelEvents::REQUEST, function (GetResponseEvent $event) use (&$stat) {
+        $stat[] = sprintf("Request came in %s", $event->getRequest()->getPathInfo());
+    });
+    $eventDispatcher->addListener(KernelEvents::CONTROLLER, function (FilterControllerEvent $event) use (&$stat) {
+        $stat[] = sprintf("Controller %s::%s selected", get_class($event->getController()[0]), $event->getController()[1]);
+    });
+    $eventDispatcher->addListener(KernelEvents::EXCEPTION, function (GetResponseForExceptionEvent $event) use (&$stat) {
+        $stat[] = sprintf("Exception thrown");
+    });
+    $eventDispatcher->addListener(KernelEvents::RESPONSE, function (FilterResponseEvent $event) use (&$stat) {
+        $content = $event->getResponse()->getContent();
+        $newContent = str_replace('</body>', '<pre>'.print_r($stat, 1).'</pre></body>', $content);
+        $event->getResponse()->setContent($newContent);
+    });
+    $eventDispatcher->addListener(KernelEvents::TERMINATE, function (PostResponseEvent $event) use (&$stat) {
+        //some heavy logic
+    });
+
+    return $kernel;
 }
--- web/app.php
+++ web/app.php
@@ -7,17 +7,8 @@
  */
 
 namespace {
-    use DG\SymfonyCert\Controller\CustomControllerResolver;
     use Symfony\Component\Debug\Debug;
-    use Symfony\Component\EventDispatcher\EventDispatcherInterface;
     use Symfony\Component\HttpFoundation\Request;
-    use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
-    use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
-    use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-    use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
-    use Symfony\Component\HttpKernel\Event\PostResponseEvent;
-    use Symfony\Component\HttpKernel\HttpKernel;
-    use Symfony\Component\HttpKernel\KernelEvents;
 
     require_once __DIR__ . '/../app/autoload.php';
     require __DIR__ . '/../app/constants.php';
@@ -34,28 +25,7 @@ namespace {
     $request->setSession(\DG\App\initSession());
     \DG\App\initTwig($container, $translator);
 
-    /** @var EventDispatcherInterface $eventDispatcher */
-    $eventDispatcher = $container->get('event_dispatcher');
-    $kernel = new HttpKernel($eventDispatcher, new CustomControllerResolver($container));
-
-    $stat = [];
-    $eventDispatcher->addListener(KernelEvents::REQUEST, function (GetResponseEvent $event) use (&$stat) {
-        $stat[] = sprintf("Request came in %s", $event->getRequest()->getPathInfo());
-    });
-    $eventDispatcher->addListener(KernelEvents::CONTROLLER, function (FilterControllerEvent $event) use (&$stat) {
-        $stat[] = sprintf("Controller %s::%s selected", get_class($event->getController()[0]), $event->getController()[1]);
-    });
-    $eventDispatcher->addListener(KernelEvents::EXCEPTION, function (GetResponseForExceptionEvent $event) use (&$stat) {
-        $stat[] = sprintf("Exception thrown");
-    });
-    $eventDispatcher->addListener(KernelEvents::RESPONSE, function (FilterResponseEvent $event) use (&$stat) {
-        $content = $event->getResponse()->getContent();
-        $newContent = str_replace('</body>', '<pre>'.print_r($stat, 1).'</pre></body>', $content);
-        $event->getResponse()->setContent($newContent);
-    });
-    $eventDispatcher->addListener(KernelEvents::TERMINATE, function (PostResponseEvent $event) use (&$stat) {
-        //some heavy logic
-    });
+    $kernel = \DG\App\initKernel($container);
 
     $response = $kernel->handle($request);
     $response->send();
